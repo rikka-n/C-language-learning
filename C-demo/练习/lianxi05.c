7b1f924723a90671bbe60d79bcb18aa35e26d3c2
rikka-n <3395287752@qq.com>
2026-06-27 17:35:33 +0800

    Add files via upload

---
+++ C-demo/练习/lianxi05.c
@@ -0,0 +1,24 @@
+/*
+题目描述:编写一个程序，输入两个整数，分别存放在变量x和y当中，
+        然后使用自己定义的函数swap来交换这两个变量的值。
+输入格式：输入只有一行，包括两个整数。
+输出格式：输出只有一行，也是两个整数，即交换以后的结果。
+要求:主函数负责数据的输入与输出，但不能直接交换这两个变量的值，
+    必须通过调用单独定义的函数swap来完成，而swap函数只负责交换变量的值，不能输出交换后的结果。
+*/
+#include <stdio.h>
+void jiaohuan(int* i , int* j);
+int main()
+{
+    int a,b;
+    scanf("%d %d",&a,&b);
+    jiaohuan(&a,&b);
+    printf("%d %d",a,b);
+    return 0;
+}
+void jiaohuan(int* i , int* j)
+{
+    int temp = *j;
+    *j = *i;
+    *i = temp;
+}
